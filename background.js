368f67da273b12ff99b1281a820cd99b9b801986
MikeG3 <harveymichaelgarner@hotmail.com>
2022-05-01 12:41:03 -0400

    Added JS

---
+++ background.js
@@ -0,0 +1,8 @@
+//Get window size
+let height = window.innerHeight;
+let width = window.innerWidth;
+
+document.getElementById("videocontainer").style.width = width;
+console.log("Resized width");
+document.getElementById("backgroundvideo").style.height = height;
+console.log("Resized height");
